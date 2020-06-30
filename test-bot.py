6a64fddf1420577615ea925efced2e3e4afde7a2
Mugen <mailabhranil@gmail.com>
2020-06-30 22:09:04 +0530

    Queue 100% Done

--- test-bot.py
+++ test-bot.py
@@ -1,22 +1,19 @@
-import asyncio
-import datetime
-from queue import Queue
+import queue
 import discord
 from discord.ext import commands
 import youtube_dl
 from discord.utils import get
-import pafy
-
 
 client = commands.Bot(command_prefix='.')
-players = {}
-queue = Queue()
-song_info = {}
+queue = queue.Queue()
+extracted_song = {}
+COUNT = 0
 
 
 @client.event
 async def on_ready():
-    print('Bot is ready')
+    print('Bot is Online')
+    print(f'Current Ping : {round(client.latency*1000)} ms')
 
 
 @client.command(pass_context=True)
@@ -33,40 +30,48 @@ async def leave(ctx):
         await server.disconnect()
 
 
-@client.command(pass_context=True)
+@client.command(pass_context=True, aliases=['pa', 'pau'])
 async def pause(ctx):
+
     voice = get(client.voice_clients, guild=ctx.guild)
 
     if voice and voice.is_playing():
+        print("Music paused")
         voice.pause()
         await ctx.send("Music paused")
     else:
+        print("Music not playing failed pause")
         await ctx.send("Music not playing failed pause")
 
 
-@client.command(pass_context=True)
+@client.command(pass_context=True, aliases=['r', 'res'])
 async def resume(ctx):
     voice = get(client.voice_clients, guild=ctx.guild)
-
     if voice and voice.is_paused():
+        print("Resumed music")
         voice.resume()
         await ctx.send("Resumed music")
     else:
+        print("Music is not paused")
         await ctx.send("Music is not paused")
 
 
-@client.command(pass_context=True)
+
+
+@client.command(pass_context=True, aliases=['s'])
 async def stop(ctx):
     voice = get(client.voice_clients, guild=ctx.guild)
-
+    queue.queue.clear()
     if voice and voice.is_playing():
+        print("Music stopped")
         voice.stop()
         await ctx.send("Music stopped")
     else:
+        print("No music playing failed to stop")
         await ctx.send("No music playing failed to stop")
 
 
-@client.command(pass_context=True, aliases=['song'])
+@client.command(pass_context=True, aliases=['song', 'p'])
 async def play(ctx, url: str):
     if ctx.message.author.voice:
         channel = ctx.message.author.voice.channel
@@ -75,9 +80,41 @@ async def play(ctx, url: str):
             await voice.move_to(channel)
         else:
             await channel.connect()
-        await ctx.send(f"MugenBot has joined the '{channel}' Channel")
+        await ctx.send(f"mugenMusicBot has joined the '{channel}' Channel")
 
     voice = get(client.voice_clients, guild=ctx.guild)
+    queue.queue.clear()
+    def queue_func():
+        if queue.not_empty:
+            ydl_opts = {
+                'default_search': 'auto',
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+            }
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                              'options': '-vn'}
+            URL = queue.get()
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                extracted_song = ydl.extract_info(URL, download=False)
+                title = extracted_song.get('title')
+                print(f'Extracting song : "{title}"')
+
+            voice.guild.voice_client.play(discord.FFmpegPCMAudio(extracted_song["formats"][0]["url"], **FFMPEG_OPTIONS),
+                                          after=lambda e: queue_func())
+            voice.source = discord.PCMVolumeTransformer(voice.guild.voice_client.source)
+            voice.source.volume = 10.0
+            print('beta')
+        elif not voice.is_playing():
+            queue.clear()
+            print("No songs were queued before the ending of the last song\n")
+            ctx.send('No songs were queued before the ending of the last song\n')
+            server = ctx.message.guild.voice_client
+            server.disconnect()
+
 
     ydl_opts = {
         'default_search': 'auto',
@@ -90,32 +127,20 @@ async def play(ctx, url: str):
     }
     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-    queue.empty()
-    if queue.empty():
-        queue.put(url)
-        while queue.qsize() > 0:
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                print('Extracting song')
-                extracted_song = ydl.extract_info(url, download=False)
-            voice.guild.voice_client.play(discord.FFmpegPCMAudio(extracted_song["formats"][0]["url"], **FFMPEG_OPTIONS))
-            voice.source = discord.PCMVolumeTransformer(voice.source)
-            voice.source.volume = 01.00
-            pafy_obj = pafy.new(url)
-            date_time = datetime.datetime.strptime(pafy_obj.duration, "%H:%M:%S")
-            a_timedelta = date_time - datetime.datetime(1900, 1, 1)
-            seconds = a_timedelta.total_seconds()
-            queue.get(url)
-            await asyncio.sleep(seconds)
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        extracted_song = ydl.extract_info(url, download=False)
+        title = extracted_song.get('title')
+        print(f'Extracting song : "{title}"')
+
+    voice.guild.voice_client.play(discord.FFmpegPCMAudio(extracted_song["formats"][0]["url"], **FFMPEG_OPTIONS),
+                                  after=lambda e: queue_func())
+    voice.source = discord.PCMVolumeTransformer(voice.guild.voice_client.source)
+    voice.source.volume = 10.0
 
 
-@client.command(pass_context=True, aliases=['q', 'queue', 'add'])
+@client.command(pass_context=True, aliases=['a', 'add'])
 async def add_to_queue(ctx, url: str):
     queue.put(url)
-
-
-@client.command(pass_context=True, aliases=['d', 'delete', 'remove'])
-async def delete_to_queue(ctx, url: str):
-    queue.get(url)
-
+    await ctx.send('Song Added To Playlist')
 
 client.run("NzIzOTM4NTQ4OTAxODcxNzEz.Xu47BA.bzxsTx6a4-dLaruzkzDIRZxogDM")
